a2d1809d86e17ad86e5c396215ef30b5a33ecbb3
lauraGgit <laura.gerhardt@gsa.gov>
2017-03-27 08:11:24 -0400

    Move to a class based generator, add url change and delimiter support
    - Move the generator into a class, which could be improved
    - have the url as a parameter for when the Generator class is initialized
    - add a delimited prompt (that needs further sanitization)

--- wordlist_passphrase_generator/generator.py
+++ wordlist_passphrase_generator/generator.py
@@ -1,56 +1,58 @@
-from sys import argv
 import random
 import requests
 
 # Get long word list.
-r = requests.get('https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt')
-words_txt = r.text
+class Generator:
+    def __init__(self, request_url):
+        self.request_url = request_url
+        self.code_words = []
+        self.passphrases = []
 
-def parse_txt(txt):
-  code_words = []
+    def get_wordlist(self):
+        r = requests.get(self.request_url)
+        self.words_txt = r.text
 
-  lines = txt.splitlines()
-  for code_word in lines:
-    code_words.append(code_word.split('\t'))
+    def parse_txt(self):
+        self.get_wordlist()
+        lines = self.words_txt.splitlines()
+        for code_word in lines:
+            self.code_words.append(code_word.split('\t'))
+        self.words_dictionary = dict(self.code_words)
 
-  return dict(code_words)
+    def get_random_int(self):
+        return str(random.randint(1,6))
 
-def get_random_int():
-  return str(random.randint(1,6))
+    def get_passcode(self, num_digits):
+        passcode = ''
+        for i in xrange(num_digits):
+            passcode += self.get_random_int()
+        return passcode
 
-def get_passcode(num_digits):
-  passcode = ''
+    def get_passcodes(self, num_passcodes, len_passcodes):
+        passcodes = []
+        for i in xrange(num_passcodes):
+            passcodes.append(self.get_passcode(len_passcodes))
+        return passcodes
 
-  for i in xrange(num_digits):
-    passcode += get_random_int()
+    def get_word(self, code):
+        return self.words_dictionary[code]
 
-  return passcode
+    def get_passphrase(self, pass_length, delimit_prompt):
+        self.parse_txt()
+        passcodes = self.get_passcodes(int(pass_length), 5)
 
-def get_passcodes(num_passcodes, len_passcodes):
-  passcodes = []
+        for passcode in passcodes:
+            self.passphrases.append(self.get_word(passcode))
 
-  for i in xrange(num_passcodes):
-    passcodes.append(get_passcode(len_passcodes))
+        passphrase = delimit_prompt.join(self.passphrases)
+        return passphrase
 
-  return passcodes
+    def passphrase(self):
+      print "How long do you want your passphrase to be?"
+      prompt = "Please enter a number of words: "
+      passphrase_length = int(raw_input(prompt))
 
-def get_word(code, dictionary):
-  return dictionary[code]
+      delimit_prompt = "How would you like to delimit your passphrase?"
+      pass_delimiter = str(raw_input(delimit_prompt))
 
-def get_passphrase(length):
-  dictionary = parse_txt(words_txt)
-  passcodes = get_passcodes(int(length), 5)
-  passphrases = []
-
-  for passcode in passcodes:
-    passphrases.append(get_word(passcode, dictionary))
-
-  passphrase = ' '.join(passphrases)
-  return passphrase
-
-def passphrase():
-  print "How long do you want your passphrase to be?"
-  prompt = "Please enter a number of words: "
-  passphrase_length = int(raw_input(prompt))
-
-  return get_passphrase(passphrase_length)
+      return self.get_passphrase(passphrase_length, pass_delimiter)
